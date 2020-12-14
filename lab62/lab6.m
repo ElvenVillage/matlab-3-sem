59835cca685d0c044c8200bb4bdb5ed0169d4035
ElvenVillage <37876353+ElvenVillage@users.noreply.github.com>
2020-12-14 13:14:18 +0300

    done

--- lab62/lab6.m
+++ lab62/lab6.m
@@ -1,4 +1,4 @@
-n = 20;
+n = 10;
 
 %Часть 1
 %От точности
@@ -12,7 +12,7 @@ for i = 1:n
     S(i) = i;
 end
 P = gallery('orthog', n);
-A = P'*diag(S)*P;
+A = P*diag(S)*P';
 
 
 error_norms1 = zeros(1, iterations1);
@@ -26,32 +26,29 @@ for i = 1:iterations1
     [B, Q, W] = jacobi(A, epsilon(i));
     S_n_n = diag(B);
     
-    error_norms1(i) = norm(sort(S_n_n)' - S);
+    error_norms1(i) = norm(diag(sort(S_n_n)') - diag(S), Inf);
     sweeps1(i) = W;
-    residuals1(i) = norm(S_n_n(5)*Q(:,5) - A*Q(:,5));
+    residuals1(i) = norm(A*Q-Q*diag(S_n_n), Inf);
 end
 
 
-% СДВИГ
-mu = (S(1) + S(n-1))/2;
-A = A - mu*eye(n);
-sweeps10 = zeros(1, iterations1);
-for i = 1:iterations1
-    [B, Q, W] = jacobi(A, epsilon(i));
-    S_n_n = diag(B);
-    
-    error_norms1(i) = norm(sort(S_n_n + mu*ones(n, 1))' - S);
-    sweeps10(i) = W;
-    residuals1(i) = norm(S_n_n(5)*Q(:,5) - A*Q(:,5));
-end
+% % СДВИГ
+% mu = (S(1) + S(n-1))/2;
+% A = A - mu*eye(n);
+% sweeps10 = zeros(1, iterations1);
+% for i = 1:iterations1
+%     [~, ~, W] = jacobi(A, epsilon(i));
+%     sweeps10(i) = W;
+% 
+% end
 
 figure
 loglog(epsilon, sweeps1, '-r');
-hold on
+% hold on
 grid on
-loglog(epsilon, sweeps10, '-b');
+% loglog(epsilon, sweeps10, '-b');
 title('Рис. 1 Зависимость числа итераций от точности')
-legend('без сдвига', 'сдвиг')
+% legend('без сдвига', 'сдвиг')
 xlabel('eps')
 ylabel('N')
 
@@ -60,7 +57,7 @@ loglog(epsilon, error_norms1, '-r');
 hold on
 grid on
 loglog(epsilon, residuals1, '-b');
-title('Рис. 2 Норма разности точного и вычисленного значений и норма невязки от заданной точности (сдвиг)')
+title('Рис. 2 Норма разности точного и вычисленного значений и норма невязки от заданной точности')
 legend('Ошибка', 'Невязка')
 xlabel('eps')
 
@@ -68,42 +65,54 @@ xlabel('eps')
 %Часть 2
 %От величины отделимости
 
-iterations2 = 50;
+iterations2 = 12;
 
 error_norms2 = zeros(1, iterations2);
 sweeps2 = zeros(1, iterations2);
 residuals2 = zeros(1, iterations2);
 x = zeros(1, iterations2);
 sweeps20 = zeros(1, iterations2);
+
 for k = 1:iterations2
-    S = zeros(1, n);
-    for i = 1:n
-        S(i) = i/(k*10);
+    S = ones(1, n);
+    x(k) = 1/(10^k);
+    for i = 1:(n/2)
+        S(i) = 1 + x(k);
     end
-    x(k) = 1/(k*10);
-    A = P'*diag(S)*P;
+   
+    A = P*diag(S)*P';
     
     [B, Q, W] = jacobi(A, epsilon(8));
     S_n_n = diag(B);
     S_n = sort(S_n_n);
     
-    error_norms2(k) = norm(S_n' - S);
+    error_norms2(k) = norm(S_n - sort(S'), Inf); 
+    
     sweeps2(k) = W;
-    residuals2(k) = norm(S_n_n(5)*Q(:,5) - A*Q(:,5));
+    residuals2(k) = norm(A*Q-Q*diag(S_n_n), Inf);
     
-    mu = (S(1) + S(n-1))/2;
-    A = A - mu*eye(n);
-    
-    [B, Q, W] = jacobi(A, epsilon(8));
-    sweeps20(k) = W;
 end
+
+%Сдвиг
+% for k = 1:iterations2
+%     [B, Q, W] = jacobi(A, epsilon(8));
+%     sweeps20(k) = W;
+%     S = zeros(1, n);
+%     for i = 1:n
+%         S(i) = 1 + i/(k*10);
+%     end
+%     x(k) = 1/(k*10);
+%     A = P'*diag(S)*P;
+%     [~,~,W] = jacobi(A, eps(8));
+%     sweeps20(k) = W;
+% end
 
 figure
 loglog(x, sweeps2, '-b');
 grid on
-hold on
-loglog(x, sweeps20, '-r');
-legend('без сдвига', 'сдвиг')
+% hold on
+% loglog(x, sweeps20, '-r');
+% legend('без сдвига', 'сдвиг')
 title('Рис. 3 Зависимость числа итераций от величины отделимости')
 xlabel('lambda')
 ylabel('N')
@@ -111,10 +120,8 @@ ylabel('N')
 figure 
 loglog(x, error_norms2);
 grid on
+hold on
 title('Рис. 4 Норма разности точного и вычисленного значений от величины отделимости')
-
-
-figure
 loglog(x, residuals2);
-grid on
-title('Рис. 5 Зависимость нормы невязки от величины отделимости')
+legend('норма разности', 'норма невязки')
+
